2bf2881deabf445dd642b841d779ee22f1bf4bd0
taha1999225 <62473707+taha1999225@users.noreply.github.com>
2021-12-16 09:42:34 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Todolist from "./components/Todolist";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Todolist />
+    </div>
+  );
+}
+
+export default App;
